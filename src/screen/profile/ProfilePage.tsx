dae6e0a5885f1df4c613ef0b666309910744bfef
PhuongIUer <miphuong1109@gmail.com>
2025-07-18 02:55:20 +0700

    Update API base URLs in LoginPage and ProfilePage to use localhost

--- src/screen/profile/ProfilePage.tsx
+++ src/screen/profile/ProfilePage.tsx
@@ -84,7 +84,7 @@ const Profile: React.FC = () => {
       if (currentOffice !== office) formData.append('office', currentOffice);
       
       const response = await axios.patch(
-        'http://192.168.1.9:3000/api/users/current-profile',
+        'http://localhost:3000/api/users/current-profile',
         formData,
         {
           headers: {
@@ -123,7 +123,7 @@ const Profile: React.FC = () => {
       if (!token) throw new Error('No authentication token found');
 
       const response = await axios.post(
-        'http://192.168.1.9:3000/api/auth/change-password',
+        'http://localhost:3000/api/auth/change-password',
         {
           currentPassword,
           newPassword,
