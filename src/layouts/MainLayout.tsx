7f1f41e1fa55bbb73fbc79ed11ca3ca0bba742b4
PhuongIUer <miphuong1109@gmail.com>
2025-07-18 01:10:19 +0700

    Implement authentication check in MainLayout to redirect to login if auth token is missing

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,8 +1,17 @@
 import Navbar from '../shared/Navbar';
 import { Outlet } from 'react-router-dom';
 import './MainLayout.css';
-
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 const MainLayout = () => {
+  const navigate = useNavigate();
+    useEffect(() => {
+      const authToken = localStorage.getItem('authToken');
+      if (!authToken) {
+        navigate('/login');
+      }
+    }, [navigate]);
+
   return (
     <div>
       <Navbar />
