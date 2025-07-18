3785e1d64a16234416bcdf54d72483c59a0acb7a
PhuongIUer <miphuong1109@gmail.com>
2025-07-19 00:39:25 +0700

    maintain domain

--- src/common/Environment.ts
+++ src/common/Environment.ts
@@ -1,14 +1,15 @@
+import ngrokDomain from './domain.json';
+
 const EnvironmentConfig = {
   // Base URLs
-  NGROK_BASE_URL: "https://a79910d23820.ngrok-free.app/api",
-  
+  NGROK_BASE_URL: ngrokDomain.domain + "/api",
+
   // API Endpoints
   API_ENDPOINTS: {
     USERS: "/users",
     PRODUCTS: "/products",
     ORDERS: "/orders"
   },
-  
 };
 
 export default EnvironmentConfig;
